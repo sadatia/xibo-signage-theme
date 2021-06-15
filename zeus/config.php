472c5a9db3966b4c8a16cca7845f05204f87a8cd
Ashik E Sadatia <sadat.post@gmail.com>
2021-06-15 16:27:54 +0600

    Add files via upload

---
+++ zeus/config.php
@@ -0,0 +1,10 @@
+<?php
+$config = array(
+    'theme_name' => 'zeus',
+    'theme_title' => 'zeus',
+    'app_name' => 'Xibo',
+    'theme_url' => 'http://transmeet.tech/signage',
+    'cms_source_url' => 'https://github.com/sadatia/xibo-cms',
+    'cms_install_url' => 'http://xibo.org.uk/manual/en/install_windows_client.html',
+    'cms_release_notes_url' => 'http://xibo.org.uk/manual/en/release_notes.html'
+);
